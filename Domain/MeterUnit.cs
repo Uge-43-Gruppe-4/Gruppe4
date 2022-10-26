33bbee72c0d7ee046d47adcde78dcfc37526eeb6
Niclas Høgh <niclas.hogh@gmail.com>
2022-10-26 09:57:30 +0200

    MeterUnit

--- Domain/MeterUnit.cs
+++ Domain/MeterUnit.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class MeterUnit
+    public class MeterUnit
     {
+        private string meterID;
+        private List<string> meterDatas = new List<string>();
+
+        public string MeterID
+        {
+            get { return meterID; }
+            set { meterID = value; }
+        }
+
+        public List<string> MeterDatas
+        {
+            get { return meterDatas; }
+        }
+
+        public MeterUnit(string meterID, string meterDatas)
+        {
+            
+        }
     }
 }
