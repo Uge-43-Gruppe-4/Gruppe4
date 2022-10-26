f9415665ad29fbbd155f993f062653746d8cfdae
Niclas Høgh <niclas.hogh@gmail.com>
2022-10-26 10:47:38 +0200

    MeterDataRep Small update

--- Domain/MeterDataRepository.cs
+++ Domain/MeterDataRepository.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class MeterDataRepository
+    public class MeterDataRepository
     {
+        private List<MeterUnit> meterList = new List<MeterUnit>();
+
+        public List<MeterUnit> MeterList
+        {
+            get { return meterList; }
+        }
+
+        public MeterDataRepository()
+        {
+            //
+        }
     }
 }
